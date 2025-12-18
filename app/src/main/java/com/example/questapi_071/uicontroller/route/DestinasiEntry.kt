28039bcb72a3213ada30fb17bd50006d027ef817
Dhnnz12 <dhonanhibrizi123@gmail.com>
2025-12-18 13:12:41 +0700

    Menambahkan DestinasiEntry

---
+++ app/src/main/java/com/example/questapi_071/uicontroller/route/DestinasiEntry.kt
@@ -0,0 +1,8 @@
+package com.example.questapi_071.uicontroller.route
+
+import com.example.questapi_071.R
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
