236bc9f0513daaef0043ad67a74fcb3060c924dd
Dhnnz12 <dhonanhibrizi123@gmail.com>
2025-12-18 11:58:08 +0700

    menambahkan fun detailsiswa dan datasiswa

--- app/src/main/java/com/example/questapi_071/modeldata/DataSiswa.kt
+++ app/src/main/java/com/example/questapi_071/modeldata/DataSiswa.kt
@@ -22,3 +22,16 @@ data class DetailSiswa(
     val alamat: String ="",
     val telpon: String ="",
 )
+
+fun DetailSiswa.toDataSiswa(): DataSiswa = DataSiswa(
+    id = id,
+    nama =nama,
+    alamat=alamat,
+    telpon=telpon
+)
+
+fun DataSiswa.toUiStateSiswa(isEntryValid: Boolean = false): UIStateSiswa =
+    UIStateSiswa(
+        detailSiswa = this.toDetailSiswa(),
+        isEntryValid = isEntryValid
+    )
