dec68bb3cda2e118ad9ea10f4af5267ea4c098d4
Dhnnz12 <dhonanhibrizi123@gmail.com>
2025-12-18 13:12:26 +0700

    Menambahkan PetaNavigasi.kt

---
+++ app/src/main/java/com/example/questapi_071/uicontroller/PetaNavigasi.kt
@@ -0,0 +1,35 @@
+package com.example.questapi_071.uicontroller
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun DataSiswaApp(navController: NavHostController = rememberNavController(),
+                 modifier: Modifier
+){
+    HostNavigasi(navController = navController)
+}
+
+@Composable
+fun HostNavigasi(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+){
+    NavHost(navController = navController, startDestination = DestinasiHome.route,
+        modifier = Modifier ){
+        composable(DestinasiHome.route) {
+            HomeScreen(navigateToItemEntry = { navController.navigate()
+                (DestinasiEntry.route) },
+                navigateToItemUpdate = {
+//                  navController.navigate("${DestinasiDetail.route}/${it}")
+                })
+        }
+        composable(DestinasiEntry.route){
+            EntrySiswaScreen(navigateBack = { navController.navigate(DestinasiHome
+                .route) })
+        }
+    }
+}
